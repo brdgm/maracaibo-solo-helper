58c362316f1b8804d9d8c31c29be256f852751bc
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-09-15 21:31:58 +0200

    combat

--- src/components/jean/turn/action/ActionCombat.vue
+++ src/components/jean/turn/action/ActionCombat.vue
@@ -10,27 +10,19 @@
             <AppIcon name="combat-nation-tiebreak" class="icon combat-nation-tiebreak"/>
           </li>
           <li><AppIcon name="combat-annex-city" class="icon combat-annex-city"/></li>
-          <li><AppIcon name="combat-annex-village" class="icon combat-annex-village"/></li>
+          <li v-if="action.combatAdditionalVillage"><AppIcon name="combat-annex-village" class="icon combat-annex-village"/></li>
           <li><AppIcon type="influence" name="any" class="icon influence" v-for="i in action.influence" :key="i"/></li>
         </ol>
       </div>
     </template>
     <template #instruction>
       <ul class="instruction">
-        <li>Reveal a combat token.</li>
-        <li>Determine the modified combat value for each nation. For each nation, first add Jean's noble rank with that nation to the combat value indicated by the combat token.
-          Then subtract your noble rank with that nation from the total. Noble ranks are the last red band you or Jean reached on the corresponding influence track.
-          If the combat token shows a requirement that modifies a nation's combat value, then apply it. Jean ignores any costs or immediate bonuses on the combat token.
-        </li>
-        <li>In case of a tie, Jean chooses the nation with the highest combat value on the combat token (ignoring any modifications or bonuses).
-          If there is still a tie, she preferentially chooses France, then Spain and finally England.
-        </li>
-        <li>Jean now annexes the lowest-numbered, empty city (without receiving the reward shown on the flag of this city).
-          If there aren't any empty cities, she annexes the lowest-numbered empty village instead.
-          If there aren't any of those either, then she removes the ownership marker from the game.
-        </li>
-        <li>Jean annexes an <b>additional empty village</b> according to the rules above.</li>
-        <li>Finally, Jeans gets the indicated influence with the nation she fought with.</li>
+        <li v-html="t('jean.action.combat.instruction.token')"></li>
+        <li v-html="t('jean.action.combat.instruction.modifyValue')"></li>
+        <li v-html="t('jean.action.combat.instruction.tie')"></li>
+        <li v-html="t('jean.action.combat.instruction.annexCity')"></li>
+        <li v-if="action.combatAdditionalVillage" v-html="t('jean.action.combat.instruction.annexVillage')" class="fw-bold"></li>
+        <li v-html="t('jean.action.combat.instruction.influence')"></li>
       </ul>
     </template>
   </ActionBox>
@@ -75,6 +67,7 @@ export default defineComponent({
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: -0.75rem;
   ol li {
     margin-top: 0.6rem;
   }
